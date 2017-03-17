032131206b020256720881a1a58a289bd1aa84bc
hk1459 <gusrbdia66@naver.com>
2017-03-17 15:07:21 +0900

    실습과제 하는중 RelativeLayout 완료 학점계산기 완료 처음에 버튼을 통하여 접근하게 만듬.

---
+++ app/src/main/java/com/example/kimja/secondhomework/Main3Activity.java
@@ -0,0 +1,90 @@
+package com.example.kimja.secondhomework;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Main3Activity extends AppCompatActivity {
+    Button b1,b2;
+    EditText editText1,editText2,editText3;
+    TextView txt1,txt2;
+    ImageView img1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        setTitle("학점 계산기");
+
+        init();
+    }
+
+    void init(){
+        b1 = (Button)findViewById(R.id.button1);
+        b2 = (Button)findViewById(R.id.button2);
+        editText1 = (EditText)findViewById(R.id.editText1);
+        editText2 = (EditText)findViewById(R.id.editText2);
+        editText3 = (EditText)findViewById(R.id.editText3);
+        txt1 = (TextView)findViewById(R.id.txt1);
+        txt2 = (TextView)findViewById(R.id.txt2);
+        img1 = (ImageView)findViewById(R.id.imageView1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inPutText1 = editText1.getText().toString();
+                String inPutText2 = editText2.getText().toString();
+                String inPutText3 = editText3.getText().toString();
+                if(inPutText1.length()==0){
+                    editText1.setText("0");
+                    inPutText1= "0";
+                }
+                if(inPutText2.length()==0){
+                    editText2.setText("0");
+                    inPutText2 = "0";
+                }
+                if(inPutText3.length()==0){
+                    editText3.setText("0");
+                    inPutText3= "0";
+                }
+                final int result,average;
+                result = Integer.parseInt(inPutText1) +Integer.parseInt(inPutText2)
+                        +Integer.parseInt(inPutText3);
+                average = (Integer.parseInt(inPutText1) +Integer.parseInt(inPutText2)
+                        +Integer.parseInt(inPutText3))/3;
+                txt1.setText(result+"점");
+                txt2.setText(average+"점");
+                if(average>=90){
+
+                }else if (average >= 80){
+                    img1.setImageDrawable(getResources().getDrawable(R.drawable.bb, getApplicationContext().getTheme()));
+                }else if (average >= 70 ){
+                    img1.setImageDrawable(getResources().getDrawable(R.drawable.cc, getApplicationContext().getTheme()));
+                }else if (average >= 60 ){
+                    img1.setImageDrawable(getResources().getDrawable(R.drawable.dd, getApplicationContext().getTheme()));
+                } else {
+                    img1.setImageDrawable(getResources().getDrawable(R.drawable.ff, getApplicationContext().getTheme()));
+                }
+                img1.setVisibility(View.VISIBLE);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText1.setText(null);
+                editText2.setText(null);
+                editText3.setText(null);
+                img1.setVisibility(View.GONE);
+                txt1.setText("0점");
+                txt2.setText("0점");
+            }
+        });
+
+
+    }
+}
